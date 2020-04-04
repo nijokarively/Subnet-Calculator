df6ae7e009d45441560430b6ffd160b37d5c6026
Nijo <nijo.karivelygmail.com>
2020-04-04 20:32:48 +0100

    added binary conversion fuctionalities

--- subnetCalculator/src/app/tab1/tab1.page.ts
+++ subnetCalculator/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public toastController: ToastController) { }
+
+  @ViewChild('inputConverterBinary', { read: ElementRef, static: false }) inputConverterBinary: ElementRef;
+  @ViewChild('inputConverterAddress', { read: ElementRef, static: false }) inputConverterAddress: ElementRef;
+
+  async createToast(msg: string) {
+    console.log(msg);
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  Convert2Binary() {
+    
+    const inputAddress = this.inputConverterAddress.nativeElement.value;
+
+    if (this.ValidateIPaddress(inputAddress)){
+      const octects = inputAddress.split('.');
+
+      const binaryOctects = octects.map(function (el) {
+        let binaryEl = parseInt(el).toString(2);
+        for (var i = binaryEl.length; i < 8; i++) {
+          binaryEl = '0' + binaryEl;
+        }
+        return binaryEl;
+      });
+  
+      const binaryAddress = binaryOctects.join('.');
+  
+      this.inputConverterBinary.nativeElement.value = binaryAddress;
+    } else {
+      this.createToast("You have entered an invalid IP address!");
+    }
+  }
+
+  ValidateIPaddress(ipAddress) {  
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipAddress)) {  
+      return (true)  
+    }  
+    return (false)  
+  }
 
 }
